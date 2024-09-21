be995a44933785788680c7829fcdc22ee553030c
rajvindergithub <rajvindergithub@gmail.com>
2024-09-21 17:01:41 +0530

    contact from with carbon field

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php 
+
+/*
+ * Plugin Name:       Conctact Form Plugin
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.10.3
+ * Requires PHP:      7.2
+ * Author:            John Smith
+ */
+
+
+
+if( !define('ABSPATH')){
+    exit(); 
+}
+
+class ContactPlugin{
+    
+    public function __construct(){
+        
+    }
+    
+    
+}
+
+
+new ContactPlugin; 
+
+
+?>
+
+
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'htmlburger/carbon-fields' => array(
+            'pretty_version' => 'v3.6.5',
+            'version' => '3.6.5.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../htmlburger/carbon-fields',
+            'aliases' => array(),
+            'reference' => 'b18f8e2a2cc4cc976306eb867ad1d740a8e7c1d9',
+            'dev_requirement' => false,
+        ),
+    ),
+);
